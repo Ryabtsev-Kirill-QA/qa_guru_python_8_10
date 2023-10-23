656646785b636a889e2413215ad1251a395d2613
Kirill <ryabtsev231297@gmail.com>
2023-10-23 09:06:10 +0300

    wip

--- qa_guru_python_8_10/pages/registration_page.py
+++ qa_guru_python_8_10/pages/registration_page.py
@@ -18,47 +18,60 @@ class RegistrationPage:
 
     def fill_first_name(self, value):
         self.first_name.type(value)
+        return self
 
     def fill_last_name(self, value):
         self.last_name.type(value)
+        return self
 
     def fill_email(self, value):
         self.email.type(value)
+        return self
 
     def choose_gender(self, checkbox_position):
         browser.all('.custom-control-input').should(be.disabled)
         browser.element(f'#gender-radio-{checkbox_position}').double_click()
         browser.element(f'#gender-radio-{checkbox_position}').should(be.enabled)
+        return self
 
     def fill_mobile_number(self, value):
         browser.element('#userNumber').type(value)
+        return self
 
     def fill_date_of_birth(self, year, month, day):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').click().element(by.text(month)).click()
         browser.element('.react-datepicker__year-select').click().element(by.text(year)).click()
         browser.element(f'.react-datepicker__day--0{day}').click()
+        return self
 
     def fill_subjects(self, value):
         browser.element('#subjectsInput').should(be.blank).type(value).press_enter()
+        return self
 
     def choose_hobbies(self, checkbox_position):
         browser.element(f'[for="hobbies-checkbox-{checkbox_position}"]').click()
+        return self
 
     def upload_picture(self, file):
         browser.element('#uploadPicture').set_value(picture.path(file))
+        return self
 
     def fill_current_address(self, value):
         browser.element('#currentAddress').type(value)
+        return self
 
     def select_state(self, value):
         browser.element('#react-select-3-input').type(value).press_enter()
+        return self
 
     def select_city(self, value):
         browser.element('#react-select-4-input').type('Delhi').press_enter()
+        return self
 
     def submit_form(self):
         browser.element('#submit').press_enter()
+        return self
 
     def should_have_registered_user_with(self, full_name, email, gender, mobile_number, day_of_birth, subjects, hobbies,
                                          picture, current_address,
--- tests/test_form_demoqa.py
+++ tests/test_form_demoqa.py
@@ -5,22 +5,23 @@ def test_form_demoqa():
     registration_page = RegistrationPage()
     registration_page.open()
 
-    # WHEN
-    registration_page.fill_first_name('Test_Name')
-    registration_page.fill_last_name('Test_Last_Name')
-    registration_page.fill_email('test@gmail.com')
-    registration_page.choose_gender('1')
-    registration_page.fill_mobile_number('8800555353')
-    registration_page.fill_date_of_birth('2000', 'December', '11')
-    registration_page.fill_subjects('Computer Science')
-    registration_page.choose_hobbies('1')
-    registration_page.upload_picture('foto.jpg')
-    registration_page.fill_current_address('Test_Adress, 9')
-    registration_page.select_state('NCR')
-    registration_page.select_city('Delhi')
-    registration_page.submit_form()
+    (
+        registration_page
+        .fill_first_name('Test_Name')
+        .fill_last_name('Test_Last_Name')
+        .fill_email('test@gmail.com')
+        .choose_gender('1')
+        .fill_mobile_number('8800555353')
+        .fill_date_of_birth('2000', 'December', '11')
+        .fill_subjects('Computer Science')
+        .choose_hobbies('1')
+        .upload_picture('foto.jpg')
+        .fill_current_address('Test_Adress, 9')
+        .select_state('NCR')
+        .select_city('Delhi')
+        .submit_form()
+    )
 
-    # THEN
     registration_page.should_have_registered_user_with(
         'Test_Name Test_Last_Name',
         'test@gmail.com',
